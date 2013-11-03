136ddb0efb7802948d90db66f1e41d8c4bb7d02d
John Bernier <jbernie2@cs.rochester.edu>
2013-11-03 13:03:13 -0500

    finished writing apply_constraint, now I just need to do testing

--- create_constraints.js
+++ create_constraints.js
@@ -1,6 +1,4 @@
-//TODO : write apply_constraint, which takes a generalized rule or rules, and 
-//applys them to the appropriate locations in the solution matrix
-
+//TODO: TEST EVERYTHING!!!!!
 
 $(document).ready(function(){
  
@@ -74,86 +72,87 @@ $(document).ready(function(){
 
     function check_interval(env,intervals,options)
     {
-        return function(id,chord1,voice1,chord2,voice2){
+        return function(chord1,voice1,chord2,voice2){
+            return function(id){
+                var constraint_lookup_by_position = 
+                    env['constraint_lookup_by_position'];
+                constraint_lookup_by_position[i][j].push(id);
 
-            var constraint_lookup_by_position = 
-                env['constraint_lookup_by_position'];
-            constraint_lookup_by_position[i][j].push(id);
-
-            if(options['greater_than'] == true){
-                return function(matrix){
-                    detect_backtrack();
-                    var interval = get_interval(matrix);
-                    if(interval > intervals[0])
-                        return check_compound_constraint(id,result);
-                }
-            }
-            else if(options['less_than'] == true){
-               return function(matrix){
-                    detect_backtrack();
-                    var interval = get_interval(matrix);
-                    if(interval < intervals[0])
-                        return check_compound_constraint(id,result);
-                } 
-            }
-            else{
-               return function(matrix){
-                    detect_backtrack();
-                    var interval = get_interval(matrix);
-                    for(var i = 0; i < intervals.length; i++){
-                        if(interval == intervals[i])
+                if(options['greater_than'] == true){
+                    return function(matrix){
+                        detect_backtrack();
+                        var interval = get_interval(matrix);
+                        if(interval > intervals[0])
                             return check_compound_constraint(id,result);
                     }
-                } 
-            }
-            function get_interval(matrix){
-                return (abs(matrix[chord2][voice2] - matrix[chord1][voice1])) 
-            }
-            function detect_backtrack(){
+                }
+                else if(options['less_than'] == true){
+                   return function(matrix){
+                        detect_backtrack();
+                        var interval = get_interval(matrix);
+                        if(interval < intervals[0])
+                            return check_compound_constraint(id,result);
+                    } 
+                }
+                else{
+                   return function(matrix){
+                        detect_backtrack();
+                        var interval = get_interval(matrix);
+                        for(var i = 0; i < intervals.length; i++){
+                            if(interval == intervals[i])
+                                return check_compound_constraint(id,result);
+                        }
+                    } 
+                }
+                function get_interval(matrix){
+                    return (abs(matrix[chord2][voice2] - matrix[chord1][voice1])) 
+                }
+                function detect_backtrack(){
 
-                var constraint_result_lookup = env['constraint_lookup'];
-                var last_chord = env['last_chord'];
-                var last_voice = env['last_voice'];
+                    var constraint_result_lookup = env['constraint_lookup'];
+                    var last_chord = env['last_chord'];
+                    var last_voice = env['last_voice'];
 
-                if(last_chord > chord){
-                    for(var i = 0, i <= last_voice; i++)
-                    {
-                        reset_constraints(
-                            constraint_lookup_by_position[last_chord][i]);
-                    }
-                }else if(last_voice > voice){
-                    for(var i = voice+1, i <= last_voice; i++)
-                    {
-                        reset_constraints(
-                            constraint_lookup_by_position[chord][i]);
-                    }
-                } 
-                function reset_constraints(constraint_id_list){
-                    for(var i = 0; i < constraint_id_list.length; i++){
-                        var id = constraint_id_list[i];
-                        constraint_result_lookup[i] = "true";
+                    if(last_chord > chord){
+                        for(var i = 0, i <= last_voice; i++)
+                        {
+                            reset_constraints(
+                                constraint_lookup_by_position[last_chord][i]);
+                        }
+                    }else if(last_voice > voice){
+                        for(var i = voice+1, i <= last_voice; i++)
+                        {
+                            reset_constraints(
+                                constraint_lookup_by_position[chord][i]);
+                        }
+                    } 
+                    function reset_constraints(constraint_id_list){
+                        for(var i = 0; i < constraint_id_list.length; i++){
+                            var id = constraint_id_list[i];
+                            constraint_result_lookup[i] = "true";
+                        }
                     }
                 }
-            }
-            function check_compound_constraint(constraint_id,result){
-                var constraint_result_lookup = 
-                    env['constraint_result_lookup'];
-                var reverse_constraint_lookup = 
-                    env['reverse_constraint_lookup'];
-                var compound_constraint_lookup = 
-                    env['compound_constraint_lookup'];
-                var compound_constraints_ids = 
-                    reverse_constraint_lookup[contraint_id];
+                function check_compound_constraint(constraint_id,result){
+                    var constraint_result_lookup = 
+                        env['constraint_result_lookup'];
+                    var reverse_constraint_lookup = 
+                        env['reverse_constraint_lookup'];
+                    var compound_constraint_lookup = 
+                        env['compound_constraint_lookup'];
+                    var compound_constraints_ids = 
+                        reverse_constraint_lookup[contraint_id];
 
-                constraint_result_lookup[constraint_id] = result;
-                
-                for(var i = 0; i < compound_constraint_ids.length; i++){
-                    compound_constraint = 
-                        compound_constraint_lookup[compound_constraint_ids[i]];
-                    result = result && compound_constraint();
+                    constraint_result_lookup[constraint_id] = result;
+                    
+                    for(var i = 0; i < compound_constraint_ids.length; i++){
+                        compound_constraint = 
+                            compound_constraint_lookup[compound_constraint_ids[i]];
+                        result = result && compound_constraint();
+                    }
+                    return result;
                 }
-                return result;
-            }    
+            }
         }
     }
   
@@ -167,12 +166,12 @@ $(document).ready(function(){
         }
     }
  
-    function create_compound_constraint_template(env,name,constraint_names,options){
+    function create_compound_constraint_template(env,name,constraint_names){
         compound_constraint_lookup_by_name = 
             env['compound_constraint_lookup_by_name'];
         
         compound_constraint_lookup_by_name[name] = 
-            function(env,context){
+            function(env,overall_context,context){
                 
                 var next_id = env['next_id'];
                 var constraint_lookup = env['constraint_lookup'];
@@ -181,17 +180,20 @@ $(document).ready(function(){
                 var constraint_id_list = new Array();
                 var compound_constraint_id = next_id();
 
-                
+                var contraint_application_environment = apply_contraints(env,overall_context); 
 
                 for(var i = 0; i < constraint_names.length; i++){
                     var constraint = constraint_names[i];
 
-                    var constraint_id = next_id();
-                    constraint_lookup[constraint_id] = 
-                        apply_constraint(env,context[i],constraint);
-                    constraint_id_list.push(constraint_id);
+                    var applied_constraint = 
+                        constraint_application_environment(env,context[i],constraint);
 
-                    reverse_constraint_lookup[constraint_id] = compound_constraint_id;
+                    for(var j = 0; j < applied_constraints.length; j++){
+                        var constraint_id = next_id();
+                        constraint_lookup[constraint_id] = applied_constraints[i]; 
+                        constraint_id_list.push(constraint_id);
+                        reverse_constraint_lookup[constraint_id] = compound_constraint_id;
+                    }
                 }
 
                 compound_constraint_lookup[compound_constraint_id] = function(){
@@ -213,7 +215,10 @@ $(document).ready(function(){
             };
     }
 
-    //THIS NEEDS A LOT OF WORK
+    //finds the appropraite places to apply a constraint to, then returns ta function
+    //that the constraint(s) can then be passed into to be applied to those positions.
+    //since the postions found are only the first position in the sequence of valid positions,
+    //each constraint applied must be passed in with an offset from the first position.
     function apply_constraint(env,overall_context){
     
         var relevant_positions;
@@ -267,8 +272,35 @@ $(document).ready(function(){
             }
         }
 
-        return function(context,constraint){
-            
+        return function(env,context,constraint){
+            var offset = context['offset'];
+            var second_offset = context['second_offset'];
+            var from_voice = context['from_voice'];
+            var to_voice = context['to_voice'];
+            var applied_constraints = new Array();
+            var positions_applied = new Array();
+
+            function contains(arr,el){
+                for(var i = 0; i < arr.length; i++){
+                    if(arr[i] == el){
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            for(var i = 0; i < relevant_positions.length; i++){
+                for(var j = 0; j < relevant_positions[i].length; j++){
+                    if(!contains(applied_postions,relevant_positions[i][j])){
+                        applied_constraints.push(
+                                constraint[i][j]+offset,
+                                constraint[i][j]+offset+second_offset,
+                                from_voice, to_voice));
+
+                        positions_applied.push(relevant_positions[i][j]);
+                    }
+                }
+            }
         };
-    }
+    }  
 }
